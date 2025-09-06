03d6662c70bf1fc938643a859261a5dcfe581ede
Daniel Teixeira <danielteixeira@protonmail.com>
2025-09-06 14:33:36 -0300

    quinto commit

--- src/calculadora.js
+++ src/calculadora.js
@@ -8,8 +8,17 @@ function subtract(a, b) {
     return a - b;
 }
 
+// Função de divisão
+function divide(a, b) {
+    if (b === 0) {
+    throw new Error("Divisão por zero não é permitida.");
+    }
+    return a / b;
+}
+
 // Exportar as funções
 module.exports = {
     add,
-    subtract
+    subtract,
+    divide,
 };
