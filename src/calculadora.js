c0f03a90ca1dfdd3bd09442ff90bfb9c74366125
Daniel Teixeira <danielteixeira@protonmail.com>
2025-09-06 14:31:15 -0300

    terceiro commit

--- src/calculadora.js
+++ src/calculadora.js
@@ -3,7 +3,13 @@ function add(a, b) {
     return a + b;
 }   
 
+// Função de subtração
+function subtract(a, b) {
+    return a - b;
+}
+
 // Exportar as funções
 module.exports = {
-    add
+    add,
+    subtract
 };
